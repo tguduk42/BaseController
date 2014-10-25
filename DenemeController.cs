26c4178869f67ce1dcd6fd2c097f2382bd86b085
turgut <turgutguduk@gmail.com>
2014-10-25 22:24:25 +0300

    Deneme
    test code example

---
+++ DenemeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Myproject.Models;
+
+namespace Myproject.Areas.Administrator.Controllers
+{
+    public class DenemeController : BaseController<Deneme>
+    {
+       /// hepsi bu kadar :) 
+
+    }
+}
